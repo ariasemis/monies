e2310bd7c82c8dbc2fbfaa9701bf851add756fd8
Arias Emilio <ariasemis@gmail.com>
2020-03-10 18:10:04 -0300

    added ability to create money values

---
+++ src/Monies.Tests/MoneyCreationTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Monies.Tests
+{
+    public class MoneyCreationTests
+    {
+        [Fact]
+        public void Try_to_create_money_without_currency_fails()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Money<object>(100, null));
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidCases))]
+        public void Created_money_has_expected_values(decimal amount, string currency)
+        {
+            var actual = new Money<string>(amount, currency);
+
+            Assert.NotNull(actual);
+            Assert.Equal(amount, actual.Amount);
+            Assert.Equal(currency, actual.Currency);
+        }
+
+        public static IEnumerable<object[]> ValidCases => new[]
+        {
+            new object[] { 100, "$" },
+            new object[] { 3.1415m, "USD" },
+            new object[] { decimal.MaxValue, string.Empty },
+            new object[] { decimal.Zero, "EUR" },
+            new object[] { -42, "€" },
+            new object[] { -765.43m, "XAU" },
+            new object[] { decimal.MinValue, "cent" },
+        };
+    }
+}
---
+++ src/Monies/Money.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Monies
+{
+    public sealed class Money<TCurrency>
+    {
+        public Money(decimal amount, TCurrency currency)
+        {
+            if (currency == null)
+                throw new ArgumentNullException(nameof(currency));
+
+            Amount = amount;
+            Currency = currency;
+        }
+
+        public decimal Amount { get; }
+
+        public TCurrency Currency { get; }
+    }
+}
