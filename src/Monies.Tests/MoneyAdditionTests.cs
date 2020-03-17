ab2068c089ec846bf8a125b01a3a94c3af6c02c6
Arias Emilio <ariasemis@gmail.com>
2020-03-17 18:40:28 -0300

    implemented money addition
    It seems that we have reached the decimal's datatype limitations.
    
    As demostrated by the associativity property test, we can get a rounding error when operating with very precise values.
    
    This is expected, as the decimal type does not aims to eliminate the need for rounding but to minimize them.
    
    In our case however, we dont want to implement a rounding policy (at least not at this stage) because we dont know in which currency the client code can be operating with.
    
    So, in order to support every scenario, we find ourselves in the need to use a more precise datatype to handle the amount values.

---
+++ src/Monies.Tests/MoneyAdditionTests.cs
@@ -0,0 +1,79 @@
+﻿using FsCheck.Xunit;
+using Monies.Tests.Generators;
+using System;
+using Xunit;
+
+namespace Monies.Tests
+{
+    [Properties(Arbitrary = new[] { typeof(MoneyArbitrary), typeof(SameCurrencyArbitrary) }, QuietOnSuccess = true, Replay = "1614940723,296719679")]
+    public class MoneyAdditionTests
+    {
+        [Property]
+        public void Adding_x_to_y_is_the_same_as_adding_y_to_x(SameCurrency<string> monies)
+        {
+            var (x, y) = monies;
+
+            Assert.Equal(x + y, y + x);
+            Assert.Equal(x.Add(y), y.Add(x));
+        }
+
+        [Property]
+        public void Adding_3_monies_in_any_order_returns_the_same_result(SameCurrency<string> monies)
+        {
+            var (x, y, z) = monies;
+
+            Assert.Equal(x + y + z, x + (y + z));
+            Assert.Equal(x.Add(y).Add(z), x.Add(y.Add(z)));
+        }
+
+        [Property]
+        public void Adding_zero_to_a_money_returns_same_money(Money<string> x)
+        {
+            var zero = Money.Create(0, x.Currency);
+
+            Assert.Equal(x, x + zero);
+            Assert.Equal(x, zero + x);
+            Assert.Equal(x, x.Add(zero));
+            Assert.Equal(x, zero.Add(x));
+        }
+
+        [Property]
+        public void Adding_null_to_a_money_returns_null(Money<string> x)
+        {
+            Assert.Null(x.Add(null));
+            Assert.Null(x + null);
+            Assert.Null(null + x);
+        }
+
+        [Theory]
+        [InlineData(100, "$", 200, "€")]
+        [InlineData(0, "USD", 50, "EUR")]
+        [InlineData(-15, "", 13, "XXX")]
+        public void Cannot_add_monies_with_different_currencies(decimal a1, string c1, decimal a2, string c2)
+        {
+            var x = Money.Create(a1, c1);
+            var y = Money.Create(a2, c2);
+
+            Assert.Throws<InvalidOperationException>(() => x + y);
+            Assert.Throws<InvalidOperationException>(() => x.Add(y));
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Adding_x_to_y_returns_z(Money<string> x, Money<string> y, Money<string> z)
+        {
+            Assert.Equal(z, x + y);
+            Assert.Equal(z, x.Add(y));
+        }
+
+        public static TheoryData<Money<string>, Money<string>, Money<string>> TestData
+            => new TheoryData<Money<string>, Money<string>, Money<string>>
+            {
+                { Money.Create(101, "$"), Money.Create(99, "$"), Money.Create(200, "$") },
+                { Money.Create(0.05m, "USD"), Money.Create(0.025m, "USD"), Money.Create(0.075m, "USD") },
+                { Money.Create(1.999m, "EUR"), Money.Create(0.9m, "EUR"), Money.Create(2.899m, "EUR") },
+                { Money.Create(2, "XBT"), Money.Create(-1, "XBT"), Money.Create(1, "XBT") },
+                { Money.Create(-100.5m, "€"), Money.Create(-0.9m, "€"), Money.Create(-101.4m, "€") },
+            };
+    }
+}
---
+++ src/Monies/Money.Operations.cs
@@ -0,0 +1,17 @@
+﻿namespace Monies
+{
+    public partial class Money<TCurrency>
+    {
+        public static Money<TCurrency> operator +(Money<TCurrency> left, Money<TCurrency> right)
+        {
+            if (left is null || right is null)
+                return null;
+
+            AssertSameCurrency(left, right);
+
+            return Money.Create(left.Amount + right.Amount, left.Currency);
+        }
+
+        public Money<TCurrency> Add(Money<TCurrency> other) => this + other;
+    }
+}
