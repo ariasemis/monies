4dc29fa8808d56197a9d97143db31f0e4598bba2
Arias Emilio <ariasemis@gmail.com>
2020-05-08 18:42:18 -0300

    parameterized rational comparison tests

--- src/Monies.Tests/Tests/Rational/RationalComparisonTests.cs
+++ src/Monies.Tests/Tests/Rational/RationalComparisonTests.cs
@@ -1,11 +1,33 @@
 ﻿using Monies.Internal;
 using Xunit;
 
-namespace Monies.Tests.Tests
+namespace Monies.Tests
 {
     public sealed class RationalComparisonTests
     {
-        // TODO: use parameterized tests
+        public static TheoryData<Rational, Rational> SameValues => new TheoryData<Rational, Rational>
+        {
+            { new Rational(1, 3), new Rational(1, 3) },
+            { new Rational(1, 2), new Rational(2, 4) },
+            { new Rational(2, 4), new Rational(4, 8) },
+            { new Rational(-11, 5), new Rational(11, -5) },
+            { new Rational(0, 1), new Rational(0, 10) },
+        };
+
+        public static TheoryData<Rational, Rational> DifferentValues => new TheoryData<Rational, Rational>
+        {
+            { new Rational(3, 5), new Rational(2, 7) },
+            { new Rational(1, 2), new Rational(-1, 2) },
+            { new Rational(-1, 3), new Rational(-1, 1) },
+            { new Rational(0, 1), new Rational(1, 0) },
+        };
+
+        public static TheoryData<Rational, Rational> LesserValues => new TheoryData<Rational, Rational>
+        {
+            { new Rational(1, 3), new Rational(1, 2) },
+            { new Rational(-1, 1), new Rational(1, 1) },
+            { new Rational(-2, 3), new Rational(-1, 3) },
+        };
 
         [Fact]
         public void Rational_is_equal_to_itself()
@@ -22,12 +44,10 @@ namespace Monies.Tests.Tests
             Assert.Equal(0, x.CompareTo(y));
         }
 
-        [Fact]
-        public void Rational_are_equivalent_if_represent_the_same_value()
+        [Theory]
+        [MemberData(nameof(SameValues))]
+        public void Rational_are_equivalent_if_represent_the_same_value(Rational x, Rational y)
         {
-            var x = new Rational(1, 2);
-            var y = new Rational(2, 4);
-
             Assert.True(Equals(x, y));
             Assert.True(x == y);
             Assert.True(x >= y);
@@ -38,12 +58,10 @@ namespace Monies.Tests.Tests
             Assert.Equal(x.GetHashCode(), y.GetHashCode());
         }
 
-        [Fact]
-        public void Rational_are_not_equal_if_different_values()
+        [Theory]
+        [MemberData(nameof(DifferentValues))]
+        public void Rational_are_not_equal_if_different_values(Rational x, Rational y)
         {
-            var x = new Rational(3, 5);
-            var y = new Rational(2, 7);
-
             Assert.True(x != y);
             Assert.False(x == y);
             Assert.False(Equals(x, y));
@@ -62,23 +80,21 @@ namespace Monies.Tests.Tests
             Assert.False(x.Equals(y));
         }
 
-        [Fact]
-        public void At_least_one_rational_must_be_lt_or_eq_the_other()
+        [Theory]
+        [MemberData(nameof(SameValues))]
+        [MemberData(nameof(DifferentValues))]
+        public void At_least_one_rational_must_be_lt_or_eq_the_other(Rational x, Rational y)
         {
-            var x = new Rational(2, 3);
-            var y = new Rational(3, 4);
-
             Assert.True(x.CompareTo(y) <= 0 || y.CompareTo(x) <= 0);
             Assert.True(x <= y || y <= x);
             Assert.True(x >= y || y >= x);
         }
 
-        [Fact]
-        public void If_not_equal_then_x_to_y_is_opposite_than_y_to_x()
+        [Theory]
+        [MemberData(nameof(DifferentValues))]
+        [MemberData(nameof(LesserValues))]
+        public void If_not_equal_then_x_to_y_is_opposite_than_y_to_x(Rational x, Rational y)
         {
-            var x = new Rational(1, 5);
-            var y = new Rational(6, 3);
-
             Assert.Equal(x.CompareTo(y), y.CompareTo(x) * -1);
             Assert.True(x < y != y < x);
             Assert.True(x <= y != y <= x);
@@ -86,12 +102,10 @@ namespace Monies.Tests.Tests
             Assert.True(x >= y != y >= x);
         }
 
-        [Fact]
-        public void Rational_x_is_less_than_y()
+        [Theory]
+        [MemberData(nameof(LesserValues))]
+        public void Rational_x_is_less_than_y(Rational x, Rational y)
         {
-            var x = new Rational(1, 3);
-            var y = new Rational(1, 2);
-
             Assert.True(x.CompareTo(y) < 0);
             Assert.True(x < y);
             Assert.True(x <= y);
