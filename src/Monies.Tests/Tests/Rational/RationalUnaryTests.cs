f66c5bc2efc6e1ae126c8d1e85815206481f229d
Arias Emilio <ariasemis@gmail.com>
2020-05-12 19:46:19 -0300

    implemented rational negation

---
+++ src/Monies.Tests/Tests/Rational/RationalUnaryTests.cs
@@ -0,0 +1,25 @@
+﻿using FsCheck.Xunit;
+using Monies.Internal;
+using System;
+using Xunit;
+
+namespace Monies.Tests
+{
+    public class RationalUnaryTests
+    {
+        [Property]
+        public void Negating_rational_works(Rational rational)
+        {
+            var numerator = Math.Abs(rational.Numerator);
+            var denominator = rational.Denominator;
+            var sign = rational.Sign * -1;
+
+            Assert.Equal(numerator, Math.Abs((-rational).Numerator));
+            Assert.Equal(denominator, (-rational).Denominator);
+            Assert.Equal(numerator, Math.Abs(Rational.Negate(rational).Numerator));
+            Assert.Equal(denominator, Rational.Negate(rational).Denominator);
+            Assert.Equal(sign, (-rational).Sign);
+            Assert.Equal(sign, Rational.Negate(rational).Sign);
+        }
+    }
+}
---
+++ src/Monies/Internal/Rational.Math.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Monies.Internal
+{
+    public partial struct Rational
+    {
+        public static Rational operator -(Rational rational)
+            => new Rational(-rational.Numerator, rational.Denominator);
+
+        public static Rational Negate(Rational item) => -item;
+
+    }
+}
