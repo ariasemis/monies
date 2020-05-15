cdc67bfadcb00297cf4290519e6b03f042107270
Arias Emilio <ariasemis@gmail.com>
2020-05-15 20:22:30 -0300

    signature and tests for converting between rationals and decimals

---
+++ src/Monies.Tests/Tests/Rational/RationalConversionTests.cs
@@ -0,0 +1,34 @@
+﻿using FsCheck.Xunit;
+using Monies.Internal;
+using Monies.Tests.Generators;
+using System;
+using Xunit;
+
+namespace Monies.Tests
+{
+    public class RationalConversionTests
+    {
+        [Property]
+        public void Converting_from_decimal_and_back_again_returns_the_original_value(decimal x)
+        {
+            Assert.Equal(x, (decimal)(Rational)x);
+            Assert.Equal(x, Rational.ToDecimal(Rational.FromDecimal(x)));
+        }
+
+        [Property(Arbitrary = new[] { typeof(NonZeroDenominatorArbitrary) })]
+        public void Converting_to_decimal_and_back_again_returns_the_original_value(Rational x)
+        {
+            Assert.Equal(x, (Rational)(decimal)x);
+            Assert.Equal(x, Rational.FromDecimal(Rational.ToDecimal(x)));
+        }
+
+        [Fact]
+        public void Cannot_convert_to_decimal_if_denominator_is_zero()
+        {
+            var x = new Rational(1, 0);
+
+            Assert.Throws<DivideByZeroException>(() => (decimal)x);
+            Assert.Throws<DivideByZeroException>(() => Rational.ToDecimal(x));
+        }
+    }
+}
---
+++ src/Monies/Internal/Rational.Conversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Monies.Internal
+{
+    public partial struct Rational
+    {
+        public static explicit operator Rational(decimal value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static explicit operator decimal(Rational value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Rational FromDecimal(decimal value) => (Rational)value;
+
+        public static decimal ToDecimal(Rational value) => (decimal)value;
+    }
+}
