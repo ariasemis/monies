0090b91ac83657998688d0698ce951a1a7615574
Arias Emilio <ariasemis@gmail.com>
2020-05-26 20:33:17 -0300

    implemented money formatting

--- src/Monies/Money/Money.Format.cs
+++ src/Monies/Money/Money.Format.cs
@@ -1,25 +1,39 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Monies
 {
     public sealed partial class Money<TCurrency> : IFormattable
     {
         public override string ToString()
-            => $"{Amount} ({Currency})";
+            => ToString("G", CultureInfo.CurrentCulture);
 
         public string ToString(string format)
-        {
-            throw new NotImplementedException();
-        }
+            => ToString(format, CultureInfo.CurrentCulture);
 
         public string ToString(IFormatProvider provider)
-        {
-            throw new NotImplementedException();
-        }
+            => ToString("G", provider);
 
         public string ToString(string format, IFormatProvider formatProvider)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(format) || format.Equals("G", StringComparison.InvariantCultureIgnoreCase))
+                format = "C";
+
+            NumberFormatInfo numberFormatInfo = null;
+
+            if (formatProvider != null)
+                numberFormatInfo = (NumberFormatInfo)formatProvider.GetFormat(typeof(NumberFormatInfo));
+
+            if (numberFormatInfo == null)
+                numberFormatInfo = NumberFormatInfo.CurrentInfo;
+
+            numberFormatInfo = (NumberFormatInfo)numberFormatInfo.Clone();
+            numberFormatInfo.CurrencySymbol = Currency.ToString();
+
+            if (!format.StartsWith("C", StringComparison.InvariantCultureIgnoreCase))
+                throw new FormatException($"The '{format}' format string is not supported.");
+
+            return Amount.ToString(format, numberFormatInfo);
         }
     }
 }
