f18a71a774c58365512d80612e454ac61a508ef0
Arias Emilio <ariasemis@users.noreply.github.com>
2020-07-03 19:26:56 -0300

    implemented toString() for discrete money

---
+++ src/Monies/Discrete/Discrete.Format.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Monies
+{
+    public sealed partial class Discrete<TCurrency, TUnit>
+    {
+        // TODO: implement IFormattable
+
+        public override string ToString()
+        {
+            var numberFormatInfo = NumberFormatInfo.GetInstance(CultureInfo.CurrentCulture);
+            numberFormatInfo = (NumberFormatInfo)numberFormatInfo.Clone();
+
+            numberFormatInfo.CurrencySymbol = Currency.ToString();
+
+            return Amount.ToString("C", numberFormatInfo);
+        }
+    }
+}
