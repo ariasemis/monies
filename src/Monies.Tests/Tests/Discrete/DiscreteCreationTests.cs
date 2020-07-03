58f5ea043c879c868a65a89c75647c0d375ba425
Arias Emilio <ariasemis@users.noreply.github.com>
2020-07-03 19:03:45 -0300

    added discrete money type

---
+++ src/Monies.Tests/Tests/Discrete/DiscreteCreationTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+
+namespace Monies.Tests
+{
+    public class DiscreteCreationTests
+    {
+        [Fact]
+        public void Cannot_create_money_without_currency()
+        {
+            Assert.Throws<ArgumentNullException>(() => Money.Discrete<string, string>(100, null, "euros"));
+        }
+
+        [Fact]
+        public void Cannot_create_money_without_unit()
+        {
+            Assert.Throws<ArgumentNullException>(() => Money.Discrete<string, string>(100, "EUR", null));
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidCases))]
+        public void Created_money_has_expected_values(long amount, string currency, string unit)
+        {
+            var actual = Money.Discrete(amount, currency, unit);
+
+            Assert.NotNull(actual);
+            Assert.Equal(amount, actual.Amount);
+            Assert.Equal(currency, actual.Currency);
+            Assert.Equal(unit, actual.Unit);
+        }
+
+        public static TheoryData<long, string, string> ValidCases => new TheoryData<long, string, string>
+        {
+            { 100, "EUR", "euros" },
+            { long.MaxValue, "EUR", "cents" },
+            { 0, string.Empty, string.Empty },
+            { -100, "USD", "dollars" },
+            { long.MinValue, "ARS", "pesos" },
+        };
+    }
+}
