39600a05f9f2375982b1bdd00466355f04115622
Arias Emilio <ariasemis@users.noreply.github.com>
2020-07-06 12:56:50 -0300

    moved method

--- src/Monies/Dense.cs
+++ src/Monies/Dense.cs
@@ -19,12 +19,5 @@ namespace Monies
         public decimal Amount => (decimal)amount;
 
         public TCurrency Currency { get; }
-
-        private static void AssertSameCurrency(Dense<TCurrency> left, Dense<TCurrency> right)
-        {
-            if (!left.Currency.Equals(right.Currency))
-                throw new InvalidOperationException(
-                    $"Cannot compare monies with different currency. Expected: {left.Currency}, Actual: {right.Currency}");
-        }
     }
 }
