f1c8852a1a13d1188b02d7bddb5071d9283b6970
Arias Emilio <ariasemis@users.noreply.github.com>
2020-08-27 18:52:26 -0300

    set debugger to step through money factory methods

--- src/Monies/Money.cs
+++ src/Monies/Money.cs
@@ -1,22 +1,27 @@
 ﻿using Monies.Internal;
 using System;
+using System.Diagnostics;
 
 namespace Monies
 {
     public static class Money
     {
+        [DebuggerStepThrough]
         public static Dense<TCurrency> Dense<TCurrency>(decimal amount, TCurrency currency)
             where TCurrency : IEquatable<TCurrency>
             => new((Rational)amount, currency);
 
+        [DebuggerStepThrough]
         public static Discrete<TCurrency> Discrete<TCurrency>(long amount, TCurrency currency)
             where TCurrency : IEquatable<TCurrency>
             => Discrete(amount, currency, Unit(1, currency));
 
+        [DebuggerStepThrough]
         public static Discrete<TCurrency> Discrete<TCurrency>(long amount, TCurrency currency, Unit<TCurrency> unit)
             where TCurrency : IEquatable<TCurrency>
             => new(amount, currency, unit);
 
+        [DebuggerStepThrough]
         public static Unit<TCurrency> Unit<TCurrency>(short scale, TCurrency currency)
             where TCurrency : IEquatable<TCurrency>
             => new(scale, currency);
