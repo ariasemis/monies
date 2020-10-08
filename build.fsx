1a3d41e40b6abb5c78555b672531b3346a124ac9
Arias Emilio <ariasemis@users.noreply.github.com>
2020-10-08 19:41:23 -0300

    fake script first version

--- build.fsx
+++ build.fsx
@@ -1,23 +1,33 @@
 #r "paket:
 nuget Fake.Core.Target
+nuget Fake.IO.FileSystem
+nuget Fake.DotNet.Cli
 "
 
 #load "./.fake/build.fsx/intellisense.fsx"
 
 open Fake.Core
+open Fake.DotNet
+
+// properties
+let sln = "./src/Monies.sln"
 
 // targets
 
 Target.create "Clean" (fun _ ->
     Trace.log " --- Cleaning --- "
+    DotNet.exec id "clean" sln |> ignore
 )
 
 Target.create "Build" (fun _ ->
     Trace.log " --- Building --- "
+    // TODO: build release
+    DotNet.build id sln
 )
 
 Target.create "Test" (fun _ ->
     Trace.log " --- Running Tests --- "
+    DotNet.test id sln
 )
 
 // dependency graph
