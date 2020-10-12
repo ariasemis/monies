16641f3f0dcf80833e27eb2c2ac7873ae245efc8
Arias Emilio <9097272+ariasemis@users.noreply.github.com>
2020-10-12 22:15:02 +0000

    improved build and dependency management
    * migration from nuget to paket
    * added build scripts with FAKE
    * improved CI github action to use build script

---
+++ build.fsx
@@ -0,0 +1,78 @@
+#r "paket:
+nuget Fake.Core.Target
+nuget Fake.DotNet.Cli
+"
+
+#load "./.fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.DotNet
+
+// properties
+let sln = "./src/Monies.sln"
+
+module BuildConfiguration =
+    let get () =
+        DotNet.BuildConfiguration.fromEnvironVarOrDefault
+            "BUILD_CONFIGURATION"
+            DotNet.BuildConfiguration.Debug
+
+    let toString (config: DotNet.BuildConfiguration) =
+        match config with
+            | DotNet.BuildConfiguration.Debug -> "Debug"
+            | DotNet.BuildConfiguration.Release -> "Release"
+            | DotNet.BuildConfiguration.Custom x -> x
+
+// targets
+
+Target.create "Clean" (fun _ ->
+    Trace.log " --- Cleaning --- "
+
+    let config = BuildConfiguration.get () |> BuildConfiguration.toString
+
+    let setOptions (options: DotNet.Options) =
+        { options with
+            CustomParams = Some (sprintf "--configuration %s" config) }
+
+    DotNet.exec setOptions "clean" sln |> ignore
+)
+
+Target.create "Restore" (fun _ ->
+    Trace.log " --- Restoring Dependencies --- "
+
+    DotNet.restore id sln
+)
+
+Target.create "Build" (fun _ ->
+    Trace.log " --- Building --- "
+
+    let setOptions (options: DotNet.BuildOptions) =
+        { options with 
+            Configuration = BuildConfiguration.get ()
+            NoRestore = true }
+
+    DotNet.build setOptions sln
+)
+
+Target.create "Test" (fun _ ->
+    Trace.log " --- Running Tests --- "
+    
+    let setOptions (options: DotNet.TestOptions) =
+        { options with
+            Configuration = BuildConfiguration.get ()
+            NoBuild = true }
+
+    DotNet.test setOptions sln
+)
+
+// dependency graph
+
+open Fake.Core.TargetOperators
+
+"Clean"
+    ==> "Restore"
+    ==> "Build"
+    ==> "Test"
+
+// run
+Target.runOrDefault "Build"
